4df968d5c48a93f53d2dcf41dddf28e75aeda53b
Ryan Rife <rwrife@gmail.com>
2026-07-21 20:08:01 -0700

    Initial Commit

---
+++ NeonInvaders macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  NeonInvaders macOS
+//
+//  Created by Ryan Rife on 7/21/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
